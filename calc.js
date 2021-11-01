35779feca929395cecd86477ed093a00f113a07a
DenisOlegovich <93161722+DenisOlegovich@users.noreply.github.com>
2021-11-01 23:08:51 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,17 +1,22 @@
-function Calc(action, a , b)
-
-if(action === "sum"){
-    return a + b
-}if(action === "multi"){
-    return a * b
-}if(action === "diff"){
-    return a - b
-}if(action === "div"){
-    return a / b
-}if(action === "RemOfDiv"){
-    return a % b
-}if(action === "pow"){
-    return a ** b
-}else{
-    return "unknown operation"
-}
+function calc(action, a , b){
+    const isNotValid = (!a || !b || (typeof a != 'number') ||(typeof a != 'number'))
+    if (isNotValid){
+        return "Error"
+    }else if(action === "sum"){
+        return a + b
+    }else if(action === "multi"){
+        return a * b
+    }else if(action === "diff"){
+        return a - b
+    }else if(action === "div"){
+        return a / b
+    }else if(action === "RemOfDiv"){
+        return a % b
+    }else if(action === "pow"){
+        return a ** b
+    }else{
+        return "unknown operation"}
+    }
+    console.log(calc("multi",2,3)) // Пример умножения
+    console.log(calc("multi",2)) // Пример ошибки
+    console.log(calc("mult",2,3)) // Пример неизвестного оператора
